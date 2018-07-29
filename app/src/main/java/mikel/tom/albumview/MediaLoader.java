3a41622113a44ecb554245a2388c9727c64d12dc
mikelhpdatke <mikelhpdatke@gmail.com>
2018-07-29 17:16:15 +0700

    Initial commit

---
+++ app/src/main/java/mikel/tom/albumview/MediaLoader.java
@@ -0,0 +1,22 @@
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.yanzhenjie.album.AlbumFile;
+import com.yanzhenjie.album.AlbumLoader;
+
+import mikel.tom.albumview.R;
+
+public class MediaLoader implements AlbumLoader {
+
+    @Override
+    public void load(ImageView imageView, AlbumFile albumFile) {
+        load(imageView, albumFile.getPath());
+    }
+
+    @Override
+    public void load(ImageView imageView, String url) {
+        Glide.with(imageView.getContext())
+                .load(url)
+                .into(imageView);
+    }
+}
